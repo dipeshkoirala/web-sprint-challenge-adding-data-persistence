068237ede61638728f7284725ba8e9263ada1f0c
dipeshkoirala <dipeshkoirala@gmail.com>
2021-03-06 00:37:50 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+const express=require(`express`)
+
+const server=express()
+
+const port=4444||process.env.PORT
+
+server.listen(port,()=>{
+    console.log(`Server started on : ${port} `)
+})
